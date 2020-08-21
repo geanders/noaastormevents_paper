01130824c557f3830d23a0dbc1ce41a16059d779
Therese Kondash <theresekondash@gmail.com>
2020-08-21 14:20:31 -0600

    updated event types and plot

--- temporal_bias_event_type.R
+++ temporal_bias_event_type.R
@@ -25,4 +25,35 @@ year_events_df <- bind_rows(year_events)
 
 write.csv(year_events_df, 'year_events_data.csv', row.names = TRUE)
 
+year_events_df <- read.csv("year_events_data.csv")
+
+# Renaming event types to current NWS documentation according to @nwsinstruction
+
+year_events_df %>% 
+  mutate(EVENT_TYPE = case_when(
+    EVENT_TYPE == "THUNDERSTORM WIND/ TREE" ~ "Thunderstorm Wind", 
+    EVENT_TYPE == "THUNDERSTORM WIND/ TREES" ~ "Thunderstorm Wind",
+    EVENT_TYPE == "Hurricane" ~ "Hurricane/Typhoon",
+    EVENT_TYPE == "Hurricane (Typhoon)" ~ "Hurricane/Typhoon",
+    TRUE ~ EVENT_TYPE
+  ))
+
+
+
+
+library(ggplot2)
+
+year_events_df %>% 
+  filter(EVENT_TYPE == c("Tornado", "Thunderstorm Wind", "Heavy Rain", "Hail")) %>% 
+  filter(year!= 2020) %>% 
+  ggplot(aes(x = year, y = n)) + 
+  geom_point() + 
+  geom_line() + 
+  facet_wrap(~ EVENT_TYPE, scales = "free_y") 
+
+
+
+
+
+
 
