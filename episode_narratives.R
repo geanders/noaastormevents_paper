ddf1847892f171ac43d13f149e8d83026bd0e810
Therese Kondash <theresekondash@gmail.com>
2020-12-11 15:42:29 -0700

    joining episode narratives for figure

--- episode_narratives.R
+++ episode_narratives.R
@@ -14,8 +14,26 @@ episode_narratives <- events_2019 %>%
   select(EPISODE_ID, EPISODE_NARRATIVE) %>% 
   distinct()
 
+dendrogram_descriptions <- tribble(
+  ~EPISODE_ID, ~description,
+  133275, "Moderate to strong atmospheric river in California",
+  133801, "An atmospheric river and cold front near Mount Saint Helena",
+  133994,"An atmospheric river in the North Bay",
+  136054,"Several severe storms along southern plains",
+  135416, "Squall line of thunderstorms in South Carolina",
+  138800,"Heat wave in eastern New York",
+  135253,"971mb bomb cyclone moving out of the Rockies", 
+  138645,"Frontal boundary over the mid-Atlantic creating a
+  low pressure wave",
+  135643,"Line of thunderstorms",
+  141710,"Strong cold front and deep upper level trough in the Ozarks",
+  137355, "Multiple storms over Missouri Ozarks",
+  142499,"A powerful coastal storm along New Jersey to the Northeast"
+)
 
 
 
-
+episode_narratives %>% 
+  left_join(dendrogram_descriptions) %>% 
+  knitr::kable(col.names = c("EPISODE_ID", "EPISODE_NARRATIVE", "description"))
 
